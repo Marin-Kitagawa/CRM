4cbcf52bf05d94ae5f8280e221015c413607ebbe
Python.clj <megha__akash@outlook.com>
2020-11-14 13:22:15 +0530

    Update settings.py

--- CRM/settings.py
+++ CRM/settings.py
@@ -139,7 +139,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 STATICFILES_DIRS = [

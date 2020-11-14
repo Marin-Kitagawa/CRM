30b512b6703e90ec3ff5e8d2fb8cf4233d95128e
Python.clj <megha__akash@outlook.com>
2020-11-14 12:48:28 +0530

    Update settings.py

--- CRM/settings.py
+++ CRM/settings.py
@@ -91,6 +91,9 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation

2c1225a9895e3a27fa501b8a28692fc93de5d0d2
Python.clj <49131888+python-clojure@users.noreply.github.com>
2020-12-20 01:40:39 +0530

    Update settings.py

--- CRM/settings.py
+++ CRM/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'DB_DEMO',
         'USER': 'postgres',
-        'PASSWORD': <PASSWORD>,
+#         'PASSWORD': <PASSWORD>,
         'HOST': 'localhost',
         'PORT': '5432',
     }
@@ -142,8 +142,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = <EMAIL>
-EMAIL_HOST_PASSWORD = <PASSWORD>
+# EMAIL_HOST_USER = <EMAIL>
+# EMAIL_HOST_PASSWORD = <PASSWORD>
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
